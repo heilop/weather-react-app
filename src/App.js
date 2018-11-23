efc30bdb5f5596fa9fef879689e5cbf9d1c1b5f3
Heissen Lopez <heissen.lopez@gmail.com>
2018-11-22 19:12:06 -0500

    Initial version of Weather component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import WeatherLocation from './components/WeatherLocation';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <WeatherLocation></WeatherLocation>
           <a
             className="App-link"
             href="https://reactjs.org"
