cb3e4112946a54b48eab59c53a603b0b3faea644
Heissen Lopez <heissen.lopez@gmail.com>
2018-11-23 12:37:42 -0500

    Add WeatherExtraInfo and WeatherTemperature components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import WeatherLocation from './components/WeatherLocation';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Location = () => (
-  <div>
-    <h1>Lima</h1>
-  </div>
-);
+const Location = (props) => {
+  // Destructuring props.
+  const { city } = props;
+  return (
+    <div>
+      <h1>
+        {city}
+      </h1>
+    </div>
+  );
+};
 
 export default Location;
