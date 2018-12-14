382db5462d9a15a87b40c1f60978bf1b76937d88
Heissen Lopez <heissen.lopez@gmail.com>
2018-12-14 00:05:04 -0500

    Include Convert Units dependency.

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Location from './Location';
+import convert from 'convert-units';
 import WeatherData from './WeatherData';
 import './styles.css';
 import {
@@ -7,18 +8,16 @@ import {
   WINDY,
 } from './../../constants/weathers';
 
+const location = 'Lima, pe';
+const api_key = '2f06324e6cdc0972675772513dfaab1a';
+const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather';
+const api_wether = `${url_base_weather}?q=${location}&APPID=${api_key}`;
+
 const data = {
   temperature: 5,
   weatherState: SUN,
   humidity: 25,
   wind: '20 m/s',
-};
-
-const data2 = {
-  temperature: 15,
-  weatherState: WINDY,
-  humidity: 10,
-  wind: '10 m/s',
 };
 
 class WeatherLocation extends React.Component {
@@ -30,11 +29,42 @@ class WeatherLocation extends React.Component {
       data: data,
     };
   }
+
+  getTemp = temp_kelvin => {
+    return Number(convert(temp_kelvin).from('K').to('C').toFixed(2));
+  }
+
+  getWeatherState =  weather_data => {
+    return SUN;
+  }
+
+  getData = weather_data => {
+    const {humidity, temp} = weather_data.main;
+    const {speed} = weather_data.wind;
+    const weatherState = this.getWeatherState(weather_data);
+    const temperature = this.getTemp(temp);
+    const data = {
+      humidity,
+      temperature,
+      weatherState,
+      wind: `${speed} m/s`,
+    }
+
+    return data;
+  }
+
   handleUpdateClick = () => {
-    // Set state anywhere (out constructor) use "this.setState".
-    this.setState({
-        city: 'Amazonas',
-        data: data2,
+    fetch(api_wether).then(resolve => {
+      return resolve.json();
+    }).then(data => {
+      const newWeather = this.getData(data);
+
+      console.log(newWeather);
+      debugger;
+      // Set state anywhere (out constructor) use "this.setState".
+      this.setState({
+          data: newWeather,
+      });
     });
   }
   render () {
